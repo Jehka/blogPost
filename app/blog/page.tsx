d91dac3cb7dcda0bad67113e6049e306fdb0c171
Jehka <jehkaransingh7803@gmail.com>
2026-04-25 21:22:15 +0530

    deploy version

---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <div style={{ padding: "100px", textAlign: "center" }}>
+      <h1>Blog</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
